dd4a9e2d7ab776bf68cabf52e5448ff7a38840e9
Giwon Im <gwim1130@naver.com>
2026-04-01 17:40:24 +0900

    PostgreSQL 16용 Flyway 모듈 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
